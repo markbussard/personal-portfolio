37100c8c4a8d941fbdc68c3c8f17e2738a03a340
markbussard <markbussard@outlook.com>
2022-03-16 22:51:17 -0700

    derp

--- scripts/skillsCanvas.js
+++ scripts/skillsCanvas.js
@@ -1,5 +1,4 @@
 function main () {
-  console.log('loading canvas');
   try {
     TagCanvas.Start('skills-canvas', 'tags', {
       textFont: 'Roboto, sans-serif',
@@ -10,9 +9,9 @@ function main () {
       wheelZoom: false,
     });
   } catch (e) {
-    console.log(`Error occured while trying to load skills-canvas: ${e}`);
+    console.error(`Error occured while trying to load skills-canvas: ${e}`);
     document.getElementById('skills-canvas-container').style.display = 'none';
   }
 }
 
-window.onload = () => main();
+document.addEventListener('DOMContentLoaded', main);
